a483834e0563d405b98052d9f9c3697d489bb614
Idris Oladele Muniru <42475829+idrismunir15@users.noreply.github.com>
2024-12-31 22:10:07 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # External API endpoint
-EXTERNAL_API_URL = "https://myaifactchecker.org/factcheckAPI/"
+EXTERNAL_API_URL = "https://myapiprojects-production.up.railway.app/api/user-input/"
 
 # Twilio Credentials
 TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER")
