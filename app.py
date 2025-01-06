f54272e29fb0828e811c73bcfb3611693eaf25ba
Idris Oladele Muniru <42475829+idrismunir15@users.noreply.github.com>
2025-01-06 18:07:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ def whatsapp_reply():
         api_response = call_external_api(incoming_message, chat_session)
         response_text = api_response.get("message", "")
         
-        message = send_message_with_template(sender_number, response_text)
+        message = send_message_with_template(sender_number, incoming_message)
         chat_session.last_message_id = message.sid
         
         chat_session.conversation_history.append({
